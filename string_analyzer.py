550ea4ec83a9278aefbc63dfc20cb93b66d22160
khadija112-stack <2023100000501@seu.edu.bd>
2026-09-17 14:37:08 +0600

    Complete API integration

--- string_analyzer.py
+++ string_analyzer.py
@@ -7,11 +7,10 @@ def analyze_digits_and_case(user_input):
 
     for char in user_input:
 
-        # Count uppercase letters
         if char.isupper():
             uppercase_count += 1
 
-        # Add numeric digits
+        
         elif char.isdigit():
             digit_sum += int(char)
 
